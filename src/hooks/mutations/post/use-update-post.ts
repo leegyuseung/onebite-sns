43394e7ecfefa422095bb4c9cac2b9dd68842204
leegyuseung <lgs4002@naver.com>
2025-12-16 23:50:24 +0900

    feat : 포스트 수정 기능

---
+++ src/hooks/mutations/post/use-update-post.ts
@@ -0,0 +1,16 @@
+import { updatePost } from "@/api/post";
+import type { useMutationCallback } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+
+export function useUpdatePost(callbacks?: useMutationCallback) {
+  return useMutation({
+    mutationFn: updatePost,
+    onSuccess: () => {
+      if (callbacks?.onSuccess) callbacks.onSuccess();
+    },
+
+    onError: (error) => {
+      if (callbacks?.onError) callbacks.onError(error);
+    },
+  });
+}
