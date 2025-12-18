0da650adfa37cad1edf7bac501397df7f5fbd62b
leegyuseung <lgs4002@naver.com>
2025-12-18 21:38:48 +0900

    feat : 포스트 삭제 기능

--- src/components/post/post-item.tsx
+++ src/components/post/post-item.tsx
@@ -1,12 +1,18 @@
 import type { Post } from "@/types";
 import { HeartIcon, MessageCircle } from "lucide-react";
 import defaultAvatar from "@/assets/default-avatar.jpg";
-import { Button } from "../ui/button";
 import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 import { formatTimeAgo } from "@/lib/time";
-import EditPostItemButton from "./edit-post-item-button";
+import EditPostButton from "./edit-post-button";
+import DeletePostButton from "./delete-post-button";
+import { useSession } from "@/store/session";
 
 export default function PostItem(post: Post) {
+  const session = useSession();
+  const userId = session?.user.id;
+
+  const isMine = post.author_id === userId;
+
   return (
     <div className="flex flex-col gap-4 border-b pb-8">
       {/* 1. 유저 정보, 수정/삭제 버튼 */}
@@ -30,10 +36,12 @@ export default function PostItem(post: Post) {
 
         {/* 1-2. 수정/삭제 버튼 */}
         <div className="text-muted-foreground flex text-sm">
-          <EditPostItemButton {...post} />
-          <Button className="cursor-pointer" variant={"ghost"}>
-            삭제
-          </Button>
+          {isMine && (
+            <>
+              <EditPostButton {...post} />
+              <DeletePostButton id={post.id} />
+            </>
+          )}
         </div>
       </div>
 
