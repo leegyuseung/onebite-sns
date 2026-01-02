21d1e2918957b4874279f4ae084ba5287315df28
leegyuseung <lgs4002@naver.com>
2026-01-02 16:01:46 +0900

    feat : 댓글 구현하기 (캐시 수정, 대댓글 기능, 무한 댓글)

--- src/components/comment/comment-editor.tsx
+++ src/components/comment/comment-editor.tsx
@@ -17,13 +17,22 @@ type EditMode = {
   onClose: () => void;
 };
 
-type Props = CreateMode | EditMode;
+type ReplyMode = {
+  type: "REPLY";
+  postId: number;
+  parentCommentId: number;
+  rootCommentId: number;
+  onClose: () => void;
+};
+
+type Props = CreateMode | EditMode | ReplyMode;
 
 export default function CommentEditor(props: Props) {
   const { mutate: createComment, isPending: isCreateCommentPending } =
     useCreateComment({
       onSuccess: () => {
         setContent("");
+        if (props.type === "REPLY") props.onClose();
       },
       onError: (error) => {
         toast.error("댓글 추가에 실패했습니다.", { position: "top-center" });
@@ -59,6 +68,13 @@ export default function CommentEditor(props: Props) {
         postId: props.postId,
         content,
       });
+    } else if (props.type === "REPLY") {
+      createComment({
+        postId: props.postId,
+        content,
+        parentCommentId: props.parentCommentId,
+        rootCommentId: props.rootCommentId,
+      });
     } else {
       updateComment({
         id: props.commentId,
@@ -77,16 +93,22 @@ export default function CommentEditor(props: Props) {
         onChange={(e) => setContent(e.target.value)}
       />
       <div className="flex justify-end gap-2">
-        {props.type === "EDIT" && (
-          <Button
-            disabled={isPending}
-            variant={"outline"}
-            onClick={() => props.onClose()}
-          >
-            취소
-          </Button>
-        )}
-        <Button disabled={isPending} onClick={handleSubmitClick}>
+        {props.type === "EDIT" ||
+          (props.type === "REPLY" && (
+            <Button
+              disabled={isPending}
+              variant={"outline"}
+              onClick={() => props.onClose()}
+              className="cursor-pointer"
+            >
+              취소
+            </Button>
+          ))}
+        <Button
+          className="cursor-pointer"
+          disabled={isPending}
+          onClick={handleSubmitClick}
+        >
           작성
         </Button>
       </div>
